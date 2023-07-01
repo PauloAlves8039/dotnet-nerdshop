36cb13e36551e733b3631ecd2f0cb548e4e35af4
PauloAlves8039 <pj38alves@gmail.com>
2023-07-01 20:00:11 -0300

    Versão inicial do projeto

---
+++ src/NerdShop.WebApp/Program.cs
@@ -0,0 +1,16 @@
+using NerdShop.WebApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
