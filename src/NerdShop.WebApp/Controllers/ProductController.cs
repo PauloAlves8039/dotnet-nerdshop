90dc744d3d86ef7ba9f7b1dd54a0b5a19af7f9b4
PauloAlves8039 <pj38alves@gmail.com>
2023-07-02 15:51:29 -0300

    Criando controlador para produtos e sua view list

---
+++ src/NerdShop.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdShop.WebApp.Repositories.Interfaces;
+
+namespace NerdShop.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult List()
+        {
+            var products = _productRepository.Products;
+            return View(products);
+        }
+    }
+}
