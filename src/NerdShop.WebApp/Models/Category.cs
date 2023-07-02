45a6bed2853cb9a3e169e7ead5bd7b79714436fa
PauloAlves8039 <pj38alves@gmail.com>
2023-07-02 10:00:19 -0300

    Criando as entidades e views iniciais

---
+++ src/NerdShop.WebApp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NerdShop.WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
+        public string CategoryName { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ src/NerdShop.WebApp/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NerdShop.WebApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto deve ser informado")]
+        [Display(Name = "Nome do Produto")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O descrição do produto deve ser informada")]
+        [Display(Name = "Descrição do Produto")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Caminho da Imagem")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Produto Preferido?")]
+        public bool IsFavoriteProduct { get; set; }
+
+        [Display(Name = "Estoque")]
+        public bool InStock { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
