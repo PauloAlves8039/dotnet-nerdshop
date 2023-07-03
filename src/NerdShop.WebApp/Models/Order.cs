6c4282bdf02556cfb02724e0b5f570c3529f54ec
PauloAlves8039 <pj38alves@gmail.com>
2023-07-03 16:09:40 -0300

    Adicionando classes para pedido e detalhes do pedido

---
+++ src/NerdShop.WebApp/Models/Order.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NerdShop.WebApp.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Informe o sobrenome")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu endereço")]
+        [StringLength(100)]
+        [Display(Name = "Endereço")]
+        public string Address { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Complemento")]
+        public string Complement { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu CEP")]
+        [Display(Name = "CEP")]
+        [StringLength(10, MinimumLength = 8)]
+        public string PostCode { get; set; }
+
+        [StringLength(10)]
+        public string State { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu telefone")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Informe o email.")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            ErrorMessage = "O email não possui um formato correto")]
+        public int Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Display(Name = "Total do Pedido")]
+        public decimal TotalOrder { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Name = "Itens no Pedido")]
+        public int TotalOrderItems { get; set; }
+
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime OrderSent { get; set; }
+
+        [Display(Name = "Data Envio Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime? OrderDeliveredAt { get; set; }
+
+        public List<OrderDetail> OrderItems { get; set; }
+    }
+}
