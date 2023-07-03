820df6efbb8754bffe01bba7eac030d544146dc4
PauloAlves8039 <pj38alves@gmail.com>
2023-07-03 10:08:04 -0300

    Criando ViewComponent para exibir detalhes do carrinho

---
+++ src/NerdShop.WebApp/Components/ShoppingCartSummary.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdShop.WebApp.Models;
+using NerdShop.WebApp.ViewModels;
+
+namespace NerdShop.WebApp.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetItemInShoppingCart();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                TotalShoppingCart = _shoppingCart.GetTotalShoppingCart(),
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
