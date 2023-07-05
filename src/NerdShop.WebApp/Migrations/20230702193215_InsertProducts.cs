7fd85361434a1666135758ec7394cb80d2117d73
PauloAlves8039 <pj38alves@gmail.com>
2023-07-05 14:53:27 -0300

    Atualziando preço de produtos e adicionando screenshots

--- src/NerdShop.WebApp/Migrations/20230702193215_InsertProducts.cs
+++ src/NerdShop.WebApp/Migrations/20230702193215_InsertProducts.cs
@@ -10,7 +10,7 @@ namespace NerdShop.WebApp.Migrations
         {
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
-                "(1,'Camiseta personalizada anos 80',1, 'https://nerdstore.com.br/wp-content/uploads/2022/02/vitrine-camiseta-80s-blowup-estampa-nerdstore.jpg.webp', 1 ,'Camiseta anos 80', 79.90)");
+                "(1,'Camiseta personalizada anos 80',1, 'https://nerdstore.com.br/wp-content/uploads/2022/02/vitrine-camiseta-80s-blowup-estampa-nerdstore.jpg.webp', 1 ,'Camiseta anos 80', 75.90)");
 
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
@@ -18,7 +18,7 @@ namespace NerdShop.WebApp.Migrations
 
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
-                "(3,'Chaveiro portátil tema game',1, 'https://nerdstore.com.br/wp-content/uploads/2022/12/a639d7cab75f7d8229a18b9d73900045.jpg.webp', 1 ,'Chaveiro GameBoy', 10.00)");
+                "(3,'Chaveiro portátil tema game',1, 'https://nerdstore.com.br/wp-content/uploads/2022/12/a639d7cab75f7d8229a18b9d73900045.jpg.webp', 1 ,'Chaveiro GameBoy', 4.00)");
 
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
@@ -26,11 +26,11 @@ namespace NerdShop.WebApp.Migrations
 
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
-                "(2,'Caneca temática Senhor dos Áneis',1, 'https://nerdstore.com.br/wp-content/uploads/2023/01/caneca-a-jornada-3-1.jpg.webp', 1 ,'Caneca A Sociedade do Anel', 60.00)");
+                "(2,'Caneca temática Senhor dos Áneis',1, 'https://nerdstore.com.br/wp-content/uploads/2023/01/caneca-a-jornada-3-1.jpg.webp', 1 ,'Caneca A Sociedade do Anel', 65.90)");
 
             migrationBuilder.Sql("INSERT INTO Products(CategoryId,Description,InStock,ImageUrl,IsFavoriteProduct,Name,Price) " +
                 "VALUES" +
-                "(3,'Chaveiro portátil tema Harry Potter',1, 'https://nerdstore.com.br/wp-content/uploads/2020/11/chaveiro-varinha-das-varinhas-harry-potter-01.jpg.webp', 0 ,'Chaveiro Harry Potter', 15.90)");
+                "(3,'Chaveiro portátil tema Harry Potter',1, 'https://nerdstore.com.br/wp-content/uploads/2020/11/chaveiro-varinha-das-varinhas-harry-potter-01.jpg.webp', 0 ,'Chaveiro Harry Potter', 10.00)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
