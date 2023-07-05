4b26ebeb06e8fcc0b64924d105358b0ae6c169eb
PauloAlves8039 <pj38alves@gmail.com>
2023-07-05 14:24:41 -0300

    Criando serviço de promoções

---
+++ src/NerdShop.WebApp/Services/Promotion.cs
@@ -0,0 +1,42 @@
+﻿using NerdShop.WebApp.Models;
+using NerdShop.WebApp.Services.Promotions;
+
+namespace NerdShop.WebApp.Services
+{
+    public class Promotion : IPromotion
+    {
+        public decimal TakeTwoPayOne(bool favorite)
+        {
+            var product = new Product();
+            var total = product.Price;
+
+            var percentage = 50.00m;
+            decimal result = 0;
+
+            var getFavoriteProduct = favorite;
+
+            if (getFavoriteProduct == true)
+            {
+                result = total - (percentage * total);
+            }
+
+            return result;
+        }
+
+        public decimal PayThreeForTen(bool favorite, int quantity)
+        {
+            var product = new Product();
+            var priceProduct = product.Price;
+            var getFavoriteProduct = favorite;
+
+            var shoppingCartItem = new ShoppingCartItem();
+            var productQuantity = shoppingCartItem.Quantity;
+
+            if (productQuantity == 3 || getFavoriteProduct == true) 
+            {
+                priceProduct = 10.00m;
+            }          
+            return priceProduct;
+        }
+    }
+}
---
+++ src/NerdShop.WebApp/Services/Promotions/IPromotion.cs
@@ -0,0 +1,9 @@
+﻿namespace NerdShop.WebApp.Services.Promotions
+{
+    public interface IPromotion
+    {
+        decimal TakeTwoPayOne(bool favorite);
+
+        decimal PayThreeForTen(bool favorite, int quantity);
+    }
+}
